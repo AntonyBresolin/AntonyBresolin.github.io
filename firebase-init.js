dec57f57e1905eed64cdf9dca4888f35b15b85c8
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2022-05-29 19:32:36 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+//chave banco firebase
+//não mexe nisso
+const firebaseConfig = {
+  apiKey: "AIzaSyCRZ2yfqm-UqrvBUmzG_3o4YmO_vGGHGnY",
+  authDomain: "aondetem-65bb0.firebaseapp.com",
+  projectId: "aondetem-65bb0",
+  storageBucket: "aondetem-65bb0.appspot.com",
+  messagingSenderId: "591474947704",
+  appId: "1:591474947704:web:546d05a611f253a1351f8d",
+  measurementId: "G-EQRKK7QWCF",
+};
+firebase.initializeApp(firebaseConfig);
